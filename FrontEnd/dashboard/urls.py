08a8ac753f8dc44af42b2403c0734656a3ee40fc
Namrata-cv26 <namrata.c.vishnu@gmail.com>
2024-03-09 17:13:53 +0530

    Upload FrontEnd files

---
+++ FrontEnd/dashboard/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('index/', views.index, name='dashboard-index'),
+    #path('logout/', views.logout_view, name='dashboard-index'),
+    path('products/', views.products, name='dashboard-products'),
+    path('products/delete/<int:pk>/', views.product_delete,
+         name='dashboard-products-delete'),
+    path('products/detail/<int:pk>/', views.product_detail,
+         name='dashboard-products-detail'),
+    path('products/edit/<int:pk>/', views.product_edit,
+         name='dashboard-products-edit'),
+    path('customers/', views.customers, name='dashboard-customers'),
+    path('dashboard-nextmonth/', views.dashboard_nextmonth, name='dashboard-nextmonth'),
+    path('customers/detial/<int:pk>/', views.customer_detail,
+         name='dashboard-customer-detail'),
+    path('order/', views.order, name='dashboard-order'),
+]
